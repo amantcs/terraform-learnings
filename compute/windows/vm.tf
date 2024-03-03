ccce97182438b7c571268705707f917e0c58bb75
Amandeep Singh <samandeep540@gmail.com>
2024-03-03 17:05:35 +0530

    terraform git ignore

--- compute/windows/vm.tf
+++ compute/windows/vm.tf
@@ -68,7 +68,7 @@ resource "azurerm_public_ip" "terraform-pub-ip" {
   name                    = "terra-ip"
   location                = azurerm_resource_group.appgrp.location
   resource_group_name     = azurerm_resource_group.appgrp.name
-  allocation_method       = "Static"
+  allocation_method       = "Dynamic"
 
 }
 
