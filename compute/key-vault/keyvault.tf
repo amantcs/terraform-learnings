615f25ee5c166877827907598db93993c714233a
Amandeep Singh <samandeep540@gmail.com>
2024-03-03 22:24:07 +0530

    keyvault

---
+++ compute/key-vault/keyvault.tf
@@ -0,0 +1,26 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "terrakeyvault7865874367"
+  location                    = azurerm_resource_group.appgrp.location
+  resource_group_name         = azurerm_resource_group.appgrp.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get", "Set", "Delete"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "vmpassword" {
+  name         = "vmpassword"
+  value        = "Azure@123"
+  key_vault_id = azurerm_key_vault.example.id
+}
---
+++ compute/key-vault/vm.tf
@@ -0,0 +1,144 @@
+
+
+locals {
+  resource_group_name = "terraform-grp"
+  location = "East US"
+  common_tags = {
+    created_by = "terraform"
+    course = "Alan"
+  }
+  subnets = [
+    {
+      name           = "subnet1"
+      address_prefix = "10.0.0.0/24"
+    },
+    {
+      name           = "subnet2"
+      address_prefix = "10.0.1.0/24"
+    #security_group = azurerm_network_security_group.example.id
+    }
+  ]
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "appgrp" {
+  name     = local.resource_group_name
+  location = local.location
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "terraform-network"
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+  address_space       = ["10.0.0.0/16"]
+  #dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  
+  tags = local.common_tags
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = local.subnets[0].name
+  resource_group_name  = azurerm_resource_group.appgrp.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = [local.subnets[0].address_prefix]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = local.subnets[1].name
+  resource_group_name  = azurerm_resource_group.appgrp.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = [local.subnets[1].address_prefix]
+}
+
+resource "azurerm_network_interface" "terraforminterface" {
+  name                = "terraform-nic"
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.terraform-pub-ip.id
+  }
+}
+
+resource "azurerm_public_ip" "terraform-pub-ip" {
+  name                    = "terra-ip"
+  location                = azurerm_resource_group.appgrp.location
+  resource_group_name     = azurerm_resource_group.appgrp.name
+  allocation_method       = "Dynamic"
+
+}
+
+#network security group
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "terra-nsg"
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+
+  security_rule {
+    name                       = "RDPRule"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+#nsg association
+
+resource "azurerm_subnet_network_security_group_association" "subnet-nsg-link" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+#virtual machine
+
+resource "azurerm_windows_virtual_machine" "VM1" {
+  name                = "terraform-vm"
+  resource_group_name = azurerm_resource_group.appgrp.name
+  location            = azurerm_resource_group.appgrp.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = azurerm_key_vault_secret.vmpassword.value
+  network_interface_ids = [
+    azurerm_network_interface.terraforminterface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_managed_disk" "datadisk1" {
+  name                 = "terraform-data-disk1"
+  location             = azurerm_resource_group.appgrp.location
+  resource_group_name  = azurerm_resource_group.appgrp.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "diskattach1" {
+  managed_disk_id    = azurerm_managed_disk.datadisk1.id
+  virtual_machine_id = azurerm_windows_virtual_machine.VM1.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
